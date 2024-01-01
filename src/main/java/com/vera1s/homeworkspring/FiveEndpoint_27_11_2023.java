b6da35ba6beb255d0eaae7511036a8fde60af449
Vera <inginer719@gmail.ru>
2024-01-01 14:27:06 -0500

    is ready

--- src/main/java/com/vera1s/homeworkspring/FiveEndpoint_27_11_2023.java
+++ src/main/java/com/vera1s/homeworkspring/FiveEndpoint_27_11_2023.java
@@ -13,7 +13,17 @@ public class FiveEndpoint_27_11_2023 {
    List<Integer> counterList = new ArrayList<>();
       private int counter = 0;
 
+    @GetMapping("/message")
+    public String message() {
+        Random random = new Random();
+        boolean sentence = random.nextBoolean();
 
+        if (sentence) {
+            return "добро пожаловать";
+        } else {
+            return "мы вам не рады";
+        }
+    }
     @GetMapping("/save-and-reset")
     public String saveAndReset() {
         counterList.add(counter);
@@ -21,7 +31,6 @@ public class FiveEndpoint_27_11_2023 {
         counter = 0;
         return "Счетчик сохранен: " + previousCounter + ". Сброс счетчика.";
     }
-
     @GetMapping("/list")
     public List<Integer> getList() {
         return counterList;
@@ -36,15 +45,48 @@ public class FiveEndpoint_27_11_2023 {
         return "актуальное значение счетчика" + counter;
     }
 
-    @GetMapping("/message")
-    public String message() {
-       Random random = new Random();
-       boolean sentence = random.nextBoolean();
 
-       if (sentence) {
-           return "добро пожаловать";
-       } else {
-           return "мы вам не рады";
-       }
-    }
+//   private int counter = 0;
+//    private List<Integer> counterList = new ArrayList<>();
+//    private Map<String, Integer> messageCountMap = new HashMap<>();
+//
+//    @GetMapping("/count")
+//    public String getCount() {
+//        counter++;
+//        return "Current count: " + counter;
+//    }
+//
+//    @GetMapping("/save-and-reset")
+//    public int saveAndReset() {
+//        counterList.add(counter);
+//        int previousCounter = counter;
+//        counter = 0;
+//        return previousCounter;
+//    }
+//
+//    @GetMapping("/list")
+//    public List<Integer> getList() {
+//        return counterList;
+//    }
+//
+//    @GetMapping("/info")
+//    public String getInfo() {
+//        return "Current count: " + counter;
+//    }
+//
+//    @GetMapping("/message")
+//    public String message() {
+//        boolean sentence = Math.random() < 0.5;
+//        String message = sentence ? "добро пожаловать" : "мы вам не рады";
+//
+//        // Increment the count for the message
+//        messageCountMap.put(message, messageCountMap.getOrDefault(message, 0) + 1);
+//
+//        return message;
+//    }
+//
+//    @GetMapping("/message-counts")
+//    public Map<String, Integer> getMessageCounts() {
+//        return messageCountMap;
+//    }
 }
